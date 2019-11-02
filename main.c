38ccbfed65140cc45fcea7bea0e18db44efc21bd
Nairod <hameldorian@gmail.com>
2019-11-02 17:34:51 +0100

    scoring, menu

--- main.c
+++ main.c
@@ -2,16 +2,19 @@
 #include <string.h>
 #include <time.h>
 #include <unistd.h>
+#include <stdlib.h>
 
-void draw_logo();
+bool start();
+void menu();
 void game();
 char dir_to_char(int dir);
 
 int main(void)
 {
-    clear();
     // Initialisation
+    clear();
     initscr();
+    srand(time(NULL));
 
     // Settings
     cbreak();
@@ -19,19 +22,20 @@ int main(void)
     keypad(stdscr,TRUE);
     curs_set(FALSE);
 
-    // Logo page
-    //draw_logo();
-
-    // Start game
-    game();
+    // start screen
+    //if(start())
+        //menu();
+    game(1);
 
     endwin();
 
     return 0;
 }
 
-void draw_logo()
+bool start()
 {
+    clear();
+
     char logo[5][32] = {
         {" _____ _____ _____ _____ _____ "},
         {"|  ___|   | |  _  |  |  |  ___|"},
@@ -56,26 +60,78 @@ void draw_logo()
         usleep(1 * 100000);
     }
 
-    char buffer[] = "Press a key.";
-    mvprintw(LINES - 1, 0, "%s", buffer);
+    char text[] = "Press ENTER to continue.";
+    mvprintw(start_y + n_rows + 3, COLS/2 - strlen(text)/2, "%s", text);
 
-    getch();
-    clear();
+    int key = 0;
+    do
+    {
+        key = getch();
+    }while(key != 10 && key != 27);
+
+    if(key == 10)
+        return true;
+
+    return false;
 }
 
-int select_level()
+void menu()
 {
-    int selection = 0;
+    char choice_1[] = "Level 1";
+    char choice_2[] = "Level 2";
+    char choice_3[] = "Level 3";
+    char choice_4[] =  "Exit";
 
-    return selection;
+    int selection = 1;
+    int key = 0;
+
+    do
+    {
+        clear();
+
+        mvprintw(LINES/2 - 3, COLS/2 - strlen(choice_1)/2, "%s", choice_1);
+        mvprintw(LINES/2 - 1, COLS/2 - strlen(choice_2)/2, "%s", choice_2);
+        mvprintw(LINES/2 + 1, COLS/2 - strlen(choice_3)/2, "%s", choice_3);
+        mvprintw(LINES/2 + 3, COLS/2 - strlen(choice_4)/2, "%s", choice_4);
+
+        switch(selection)
+        {
+            case 1:
+                mvprintw(LINES/2 - 3, COLS/2 - strlen(choice_1)/2 - 5, "==>");
+                break;
+            case 2:
+                mvprintw(LINES/2 - 1, COLS/2 - strlen(choice_2)/2 - 5, "==>");
+                break;
+            case 3:
+                mvprintw(LINES/2 + 1, COLS/2 - strlen(choice_3)/2 - 5, "==>");
+                break;
+            case 4:
+                mvprintw(LINES/2 + 3, COLS/2 - strlen(choice_4)/2 - 5, "==>");
+                break;
+            default:
+                break;
+        }
+        refresh();
+
+        key = getch();
+
+        if(key == KEY_UP && selection > 1)
+            selection--;
+        else if(key == KEY_DOWN && selection < 4)
+            selection++;
+        else if(key == 10 && selection < 4)
+            game(selection);
+
+    }while((key != 10 || selection != 4) && key != 27); // 10 = "Enter" ; 27 = "ESC"
 }
 
-void game()
+void game(int level)
 {
     nodelay(stdscr, TRUE);
+    clear();
 
     // Field params
-    char field[20][40][2] = {' '}; // The 3rd dimension represents direction
+    char field[15][45][2] = {' '}; // The 3rd dimension represents direction
     int n_rows=(sizeof(field)/sizeof(field[0]));
     int n_cols=(sizeof(field)/sizeof(field[0][0]))/n_rows;
 
@@ -99,8 +155,8 @@ void game()
     attroff(A_STANDOUT);
 
     // Print info
-    char buffer[] = "Press Q to quit.";
-    mvprintw(LINES - 1, 0, "%s", buffer);
+    mvprintw(LINES - 2, 0, "P : Pause");
+    mvprintw(LINES - 1, 0, "ESC : Exit");
 
     // Snake current position
     int x = n_cols/4, y= n_rows/2;
@@ -113,20 +169,33 @@ void game()
     }
 
     // Variables
-    char head = '>', body = '0';
-    int key;
+    char head = '>', body = '0', food = '*';
+    int  y_food = -1, x_food = -1;
+    bool got_food = false;
+    int key, key_2;
     int dir = KEY_RIGHT;
+    int score = 0;
     bool game_over = false;
 
     // Game loop
     do{
         key = getch();
+        key_2 = 0;
 
-        if(
-        (key == KEY_LEFT && field[y][x-1][0] != body)
-        || (key == KEY_UP && field[y-1][x][0] != body)
-        || (key == KEY_RIGHT && field[y][x+1][0] != body)
-        || (key == KEY_DOWN && field[y+1][x][0] != body)
+        // Pause with P
+        if(key == 'p' || key == 'P')
+            do{
+                key_2 = getch();
+            }while(key_2 != 'p' && key_2 != 'P' && key_2 != 27);
+        if(key_2 == 27)
+            break;
+
+        // Change direction
+        if( // Cannot change direction to opposite
+        (key == KEY_LEFT && dir != KEY_RIGHT)
+        || (key == KEY_UP && dir != KEY_DOWN)
+        || (key == KEY_RIGHT && dir != KEY_LEFT)
+        || (key == KEY_DOWN && dir != KEY_UP)
         )
             dir = key;
 
@@ -154,46 +223,74 @@ void game()
             break;
         }
 
-        // Reassign head position
+        // Move head position
         field[y][x][0] = head;
 
-        // Reassign tail position
-        field[y_tail][x_tail][0] = ' ';
-        switch(field[y_tail][x_tail][1])
+        // Move tail position
+        if(y != y_food || x != x_food) // If on food, the tail does not move
         {
-            case 'L':
-                field[y_tail][x_tail][1] = ' ';
-                x_tail = (x_tail > 0) ? x_tail - 1 : n_cols - 1;
-                break;
-            case 'U':
-                field[y_tail][x_tail][1] = ' ';
-                y_tail = (y_tail > 0) ? y_tail - 1 : n_rows - 1;
-                break;
-            case 'R':
-                field[y_tail][x_tail][1] = ' ';
-                x_tail = (x_tail < n_cols - 1) ? x_tail + 1 : 0;
-                break;
-            case 'D':
-                field[y_tail][x_tail][1] = ' ';
-                y_tail = (y_tail < n_rows - 1) ? y_tail + 1 : 0;
-                break;
-            default:
-                break;
+            field[y_tail][x_tail][0] = ' ';
+            switch(field[y_tail][x_tail][1])
+            {
+                case 'L':
+                    field[y_tail][x_tail][1] = ' ';
+                    x_tail = (x_tail > 0) ? x_tail - 1 : n_cols - 1;
+                    break;
+                case 'U':
+                    field[y_tail][x_tail][1] = ' ';
+                    y_tail = (y_tail > 0) ? y_tail - 1 : n_rows - 1;
+                    break;
+                case 'R':
+                    field[y_tail][x_tail][1] = ' ';
+                    x_tail = (x_tail < n_cols - 1) ? x_tail + 1 : 0;
+                    break;
+                case 'D':
+                    field[y_tail][x_tail][1] = ' ';
+                    y_tail = (y_tail < n_rows - 1) ? y_tail + 1 : 0;
+                    break;
+                default:
+                    break;
+            }
         }
 
-        mvprintw(0, 0, "Y : %d", y);
-        mvprintw(1, 0, "X : %d", x);
+        // Update score
+        if(y == y_food && x == x_food)
+        {
+            score++;
+            y_food = -1;
+            x_food = -1;
+        }
+
+        // Drop food randomely
+        if(y_food == -1 || x_food == -1)
+            do
+            {
+                y_food = rand() % n_rows;
+                x_food = rand() % n_cols;
+            }while(field[y_food][x_food][0] == ' '
+                   && (y - y_food + x - x_food < level*5)); // Food pops further depending on the level
+
+        field[y_food][x_food][0] = food;
 
         // Draw field
         for(int y = 0; y < n_rows; y++)
             for(int x = 0; x < n_cols; x++)
                 mvprintw(row_min + y, col_min + x, "%c", field[y][x][0]);
 
-        refresh();
-        usleep(500000);
+        //Print score
+        attron(A_DIM);
+        mvprintw(row_start - 2, col_start, "SCORE : %d", score);
+        attroff(A_DIM);
 
-    } while(key != 'q' && key != 'Q');
-    clear();
+        refresh();
+
+        //Pause
+        int delay = 1000000 * 0.5 / level; // Speed increases with level
+        usleep(delay);
+
+    } while(key != 27);
+
+    nodelay(stdscr, FALSE);
 }
 
 char dir_to_char(int dir)
